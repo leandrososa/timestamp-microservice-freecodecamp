8541caf57383aa3ce4eec792033f116b0c1e2a1d
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-03-19 00:14:30 -0300

    changed port for production

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
