1ee4adcfaff318fa3eb07c804fbe95c3173b32c4
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-03-18 22:07:08 -0300

    added timestamp logic

--- index.js
+++ index.js
@@ -3,24 +3,52 @@ var app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    //res.send('Hello Express')
     res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/api/timestamp/', (req, res) => {
-    res.send('holis');
 
-    console.log(getTimestamp());
-    console.log(getTimestamp('2015-03-03'));
+    console.log(message(req.ip));
+
+    let date = new Date();
+    json = { 'unix': getTimestamp(date), 'utc': date.toUTCString() };
+
+    res.json(json)
 });
 
-function getTimestamp(date = new Date){
-    return Math.floor(date.getTime() / 1000)
+app.get('/api/timestamp/:date', (req, res) => {
+    console.log(message(req.ip));
+    let json;
+
+    try{
+        if(req.params.date.indexOf('-') > -1){
+            let timestamp = getTimestamp(new Date(req.params.date));
+            json = { 'unix': timestamp, 'utc': new Date(req.params.date).toUTCString() }
+        }else{
+            let int = parseInt(req.params.date);
+            json = { 'unix': int, 'utc': getUTCDate(int).toUTCString() }
+        }
+
+    } catch(err) {
+        json = { error : "Invalid Date" }
+    }
+
+    res.json(json)
+});
+
+function getTimestamp(date){
+    return date.getTime()
+}
+
+function getUTCDate(timestamp){
+    return new Date(timestamp);
+}
+
+function message(ip){
+    return `Nueva llamada desde ${ip}`;
 }
 
 
-
-
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Aplicacion ejecutandose en http://localhost:${port}`)
 })
